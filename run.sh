78dad7cecd150d1a4f9b278febaaf89b68f3be8a
Brian Dopson <brian_dopson1@homedepot.com>
2017-11-20 21:01:17 -0500

    update hostnames

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ echo '************************************************************************'
 echo "waiting for the config server to start on port $CONFIG_SERVER_PORT"
 echo '************************************************************************'
 
-while ! `nc -z config_server $CONFIG_SERVER_PORT `;
+while ! `nc -z config-server $CONFIG_SERVER_PORT `;
   [ ca ]; do sleep 3; done
 echo '>>>>>>>>>>>>>>> config server has started'
 
 echo "********************************************************"
 echo "Waiting for the eureka server to start  on port $EUREKA_SERVER_PORT"
 echo "********************************************************"
-while ! `nc -z eureka_server $EUREKA_SERVER_PORT`; do sleep 3; done
+while ! `nc -z eureka-server $EUREKA_SERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka Server has started"
 
 echo "********************************************************"
